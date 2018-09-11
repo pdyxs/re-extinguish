cb9c6b813ddc7a831b26b7062b87b1843c7c4513
Paul Sztajer <paul@sztajer.org>
2018-09-10 20:28:47 -0500

    package updates

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -13,7 +13,7 @@ export default {
     umdNamedDefine: true,
   },
   plugins: [
-    // new webpack.optimize.UglifyJsPlugin(),
+    new webpack.optimize.UglifyJsPlugin(),
     new webpack.DefinePlugin({
       'process.env.NODE_ENV': JSON.stringify('production'),
     }),
